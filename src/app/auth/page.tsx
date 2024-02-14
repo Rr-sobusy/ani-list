e62863608d4b7e4581b0d937aaef4e14875b8cb1
unknown <rockph0029@gmail.com>
2024-02-14 10:52:37 +0800

    login form

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -19,21 +19,36 @@ const LoginPage = (props: Props) => {
   }
   return (
     <form onSubmit={submitHandler} className='flex items-center justify-center h-screen bg-[#F3F4F6]'>
-      <Card className='lg:w-1/3 md:w-3/4 flex flex-col gap-5 w-full h-full md:h-[550px] px-6 py-10 md:py-8 rounded-xl'>
+      <Card className='lg:w-1/3 md:w-3/4 flex flex-col gap-5 w-full h-full md:h-[550px] px-6 py-14 md:py-8 rounded-xl'>
         <div className='flex flex-col items-center'>
-          <h2 className='scroll-m-20 text-4xl text-[#E11D48] font-extrabold tracking-tight lg:text-3xl'>Ani<span className='text-primary font-cardo tracking-wide'>List</span></h2>
+          <h2 className='scroll-m-20 text-4xl text-[#E11D48] font-extrabold tracking-tight lg:text-3xl'>Anime<span className='text-primary tracking-wide'>List</span></h2>
           <h6 className='font-sans text-gray-600 font-semibold tracking-wider'>Enter your username and password to login</h6>
         </div>
 
         {/* Input fields */}
-        <div className='flex md:mt-3 mt-8 flex-col gap-2'>
-          <label className='text-slate-700 text-lg font-semibold tracking-tight scroll-m-20'>Username</label>
+        <div className='flex flex-col md:gap-2 gap-4'>
+          <label className='text-slate-700 md:text-lg text-xl font-semibold tracking-tight scroll-m-20'>Username</label>
           <Input name='username' className='py-6 text-sm text-slate-700' placeholder='Enter your username' />
-          <label className='text-slate-700 text-lg font-semibold tracking-tight scroll-m-20'>Password</label>
+          <label className='text-slate-700 md:text-lg text-xl font-semibold tracking-tight scroll-m-20'>Password</label>
           <Input name='password' className='py-6 text-sm text-slate-700' placeholder='Enter your password' />
         </div>
-        <Button type='submit' className='py-6'>Login</Button>
-        <h5 className='font-sans text-slate-500 font-semibold text-sm'>Or login with</h5>
+        <div className='flex items-center flex-col gap-3'>
+          <Button type='submit' className='py-6 w-full'>Login</Button>
+          <Button variant="outline" type='submit' className='py-6 w-full'>Register</Button>
+        </div>
+
+        <div className='h-[12px] flex justify-center items-center'>
+          <h5 className='font-sans text-primary font-semibold text-sm grid grid-cols-3 gap-2 items-center __dashed-text'>Or continue with</h5>
+        </div>
+
+        <div className='flex gap-2 mt-3 md:mt-0'>
+          <Button className='w-full font-semibold flex gap-2' variant="outline">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" /></svg> Github
+          </Button>
+          <Button className='w-full font-semibold flex gap-2' variant="outline">
+            <img alt='' src='https://fonts.gstatic.com/s/i/productlogos/googleg/v6/24px.svg'></img>   Google
+          </Button>
+        </div>
 
       </Card>
     </form>
